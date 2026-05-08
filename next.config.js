e1db45e936e3ed04bec04140b5607036ea11a688
esako20-del <esako20@gmail.com>
2026-05-08 13:06:41 -0400

    Remove experimental serverActions from config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverActions: true },
   images: { domains: ['lh3.googleusercontent.com'] },
 }
 module.exports = nextConfig
